6428b0d2bf1457adf08535ae3486e74cc06336ff
Joe Sweeney <joe@joesweeney.org>
2017-04-22 02:39:36 -0400

    Rework AI to use a simulated annealing approach
    This needs to be tweaked, but it shows some promise.

--- ai.js
+++ ai.js
@@ -5,14 +5,35 @@ class AI {
     this.game = game
     this.moving = null
     this.possibleMoves = [0,1,2]
-    this.setUpLearning()
+    this.score = [0,0]
+    this.playing = false
+    this.k = 0
+    this.kmax = 1000
+    this.nextEnergy = null
+    this.state = [1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+    this.energy = 10
+    this.candidate = null
     console.log(game)
-    window.onload = this.startGame.bind(this)
+
+    this.loadState()
+    // window.onload = this.startGame.bind(this)
+    window.onload = this.startLearning.bind(this)
   }
 
-  getBallCoordinates() {
-    let ball = this.game.ball
-    return {x:ball.x, y:ball.y}
+  loadState() {
+    if(localStorage.getItem("pongAI")) {
+      let json = JSON.parse(localStorage.getItem("pongAI"))
+      this.energy = json.energy
+      this.state = json.state
+    }
+  }
+
+  saveState() {
+    let store = {
+      energy: this.energy,
+      state: this.state
+    }
+    localStorage.setItem("pongAI", JSON.stringify(store))
   }
 
   sendKeyDown(key) {
@@ -25,32 +46,129 @@ class AI {
     this.game.onkeyup(keycode)
   }
 
+  startLearning() {
+    console.log("learning: "+this.k)
+    if(!this.playing) {
+      this.generateCandidate()
+      this.evaluateCandidate()
+    }
+  }
+
+  playAgain() {
+    this.k += 1
+    if(this.acceptanceProb() >= Math.random()) {
+      this.state = this.candidate
+      this.energy = this.nextEnergy
+      console.log("accepted candidate, new energy is: "+this.energy)
+    }
+    if(this.k<this.kmax) {
+      this.startLearning()
+    } else {
+      this.saveState()
+    }
+  }
+
+  evaluateCandidate() {
+    this.startGame()
+  }
+
+  acceptanceProb() {
+    this.nextEnergy = this.score[1]-this.score[0]
+    let p = Math.exp(this.energy-this.nextEnergy)
+    console.log("acceptanceProb: "+p+" nextEnergy: "+this.nextEnergy)
+    return p
+  }
+
+  generateCandidate() {
+    let newCoeffs = []
+    for(let i = 0; i<this.state.length; i++) {
+      let r = Math.random()
+      let factor = 1.0
+      if(r<0.05) {
+        // Try a big jump
+        factor = (Math.random() * 100 ) - 50
+      } else {
+        // Tweak it
+        factor = Math.random()+0.5
+      }
+      newCoeffs.push(this.state[i]*factor)
+    }
+    this.candidate = newCoeffs
+  }
+
   startGame() {
+    this.playing = true
     this.sendKeyDown("1")
-    let fps = 60
-    let intervalTime = 1000.0/60
+    let fps = 1000
+    let intervalTime = 1000.0/fps
     this.intervalID = window.setInterval(this.makeMove.bind(this), intervalTime)
   }
 
+  makeMove() {
+    if(this.isTerminal()) {
+      window.clearInterval(this.intervalID)
+      this.playing = false
+      this.playAgain()
+    }
+    let up = 1, down = 2, stop = 0
+    let difference = this.updateScores()
+    let dir = this.getMove()
+    this.move(dir)
+  }
+
+  getMove() {
+    let downThreshold = -10
+    let upThreshold = 10
+    let params = this.getParameters()
+    let sum = 0
+    for(let i = 0; i<params.length; i++) {
+      sum += params[i]*this.candidate[i]
+    }
+    if(sum < downThreshold) {
+      return 2
+    } else if (sum > upThreshold) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
   // makeMove() {
-  //   let difference = this.updateScores()
-  //   let reward = this.getReward(difference)
-  //   let dir = this.getMove()
+  //   if(this.isTerminal())   {
+  //     window.clearInterval(this.intervalID)
+  //     this.playing = false
+  //   }
+  //   let paddle = this.game.leftPaddle
+  //   let ball = this.game.ball
   //   let up = 1, down = 2, stop = 0
-  //   this.move(dir)
+  //   if(paddle.y+(paddle.height/2)<ball.y) {
+  //     // move down
+  //     this.move(down)
+  //   }
+  //   else if (paddle.y+(paddle.height/2)>ball.y) {
+  //     this.move(up)
+  //   }
   // }
 
-  makeMove() {
-    let paddle = this.game.leftPaddle
+  updateScores() {
+    let newScores = this.game.scores
+    let difference = [newScores[0]-this.score[0],newScores[1]-this.score[1]]
+    if(difference[0] !== 0 || difference[1] !== 0) {
+      this.score = newScores
+    }
+    return difference
+  }
+
+  getParameters() {
     let ball = this.game.ball
-    let up = 1, down = 2, stop = 0
-    if(paddle.y+(paddle.height/2)<ball.y) {
-      // move down
-      this.move(down)
-    }
-    else if (paddle.y+(paddle.height/2)>ball.y) {
-      this.move(up)
-    }
+    let paddle = this.game.leftPaddle
+    return [ball.x, ball.y, ball.dx, ball.dy, ball.accel, paddle.x, paddle.y]
+  }
+
+
+  isTerminal() {
+    let scores = this.game.scores
+    return scores[0]===9 || scores[1]===9
   }
 
   /* Movement code */
@@ -98,104 +216,4 @@ class AI {
       this.moving === down ? this.stopMovingDown() : null
     }
   }
-
-  /* Reinforcement learning code */
-
-  setUpLearning() {
-    this.score = [0,0]
-    this.Q = {}
-    this.N = {}
-    this.lastState = [1,2,2]
-    this.lastAction = null
-    this.lastReward = null
-  }
-
-  updateScores() {
-    let newScores = this.game.scores
-    let difference = [newScores[0]-this.score[0],newScores[1]-this.score[1]]
-    if(difference[0] !== 0 || difference[1] !== 0) {
-      this.scores = newScores
-    }
-    return difference
-  }
-
-  getState() {
-    let ball = this.game.ball
-    let paddle = this.game.leftPaddle
-    return [ball.x, ball.y, ball.dx, ball.dy, ball.accel, paddle.x, paddle.y]
-  }
-
-  getReward(difference) {
-    return difference[0]+(-1*difference[1])
-  }
-
-  isTerminal() {
-    if(this.game.menu.winner === undefined) {
-      return false
-    }
-    else {
-      return true
-    }
-  }
-
-  getQ(s, a) {
-    let key = s.toString+","+a
-    let val = this.Q[key]
-    if(val !== undefined) {
-      return val
-    } else {
-      return 0
-    }
-  }
-
-  setQ(s,a, val) {
-    let key = s.toString+","+a
-    this.Q[key] = val
-  }
-
-  getN(s,a) {
-    let key = s.toString+","+a
-    let val = this.N[key]
-    if(val !== undefined) {
-      return val
-    } else {
-      return 0
-    }
-  }
-
-  incN(s,a) {
-    let key = s.toString+","+a
-    let val = this.N[key]
-    if(val !== undefined) {
-      this.N[key] = 1
-    } else {
-      this.N[key] += 1
-    }
-  }
-
-  updateQ(s,a){
-    return 0
-  }
-
-  getMaxAction(lastState, lastAction, lastReward, state) {
-    return 0
-  }
-
-  // This is an implementation of a basic Q-Learning Agent
-  getMove(reward) {
-    let state = this.getState()
-    if(this.isTerminal()) {
-      this.setQ(this.lastState,this.lastAction,reward)
-      window.clearInterval(this.intervalID)
-    }
-    if(this.previousState !== null){
-      this.incN(this.lastState,this.lastAction)
-      this.updateQ(this.lastState,this.lastAction)
-    }
-    this.lastState = state
-    this.lastAction = this.getMaxAction()
-    this.lastReward = reward
-    return 0
-  }
-
 }
